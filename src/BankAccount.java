2e8ce58e4b324cadaa1ca08752267d8b4e590cd5
Ricardo Joasilus <rjoasilus@hotmail.com>
2025-02-04 02:14:57 +0000

    Third Assignment

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,17 @@
+public class BankAccount
+{
+    public static void main(String[] args)
+    {
+        // initial balance of Bob
+        double Balance = 2175.37;
+        Balance -= 302.50;
+        Balance += 50.03;
+        Balance /= 2.00;
+        Balance += 20.00;
+        Balance -= 1.00;
+        Balance *= 2.00;
+        Balance += 1.00;
 
+        System.out.println("Bob's new balance is: " + Balance);
+    }
+}
